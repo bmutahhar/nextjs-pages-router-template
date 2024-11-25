117df8bc5b1629f8de375978b67adb7a08834d7b
Mutahhar bin Muzaffar <mutahharbinmuzaffar@gmail.com>
2024-11-25 11:31:07 +0500

    Finalized color theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
 
-export const App = ({ Component, pageProps }: AppProps) => {
+const App = ({ Component, pageProps }: AppProps) => {
   return <Component {...pageProps} />;
 };
+
+export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ const geistMono = localFont({
   weight: "100 900",
 });
 
-export const Home = () => {
+const Home = () => {
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
@@ -57,7 +57,7 @@ export const Home = () => {
           </a> */}
           <Button>Deploy now</Button>
           <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            className="rounded-md border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -116,3 +116,5 @@ export const Home = () => {
     </div>
   );
 };
+
+export default Home;
